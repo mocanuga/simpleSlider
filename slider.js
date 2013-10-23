aa3447c166004e0d4ca08db2a900fc0608201c9a
Adi <mocanugeorge@gmail.com>
2013-10-23 17:16:44 +0300

    Update slider.js
    CSS arrows added with some nifty 'follow the slide' effect

--- slider.js
+++ slider.js
@@ -7,57 +7,48 @@
  * global jQuery
  */
 (function ($) {
-    var $d = $(document),
-        $w = $(window);
-    function Slider (opts) {
+    'use strict';
+    var $d = $(document), $w = $(window);
+    function Slider(opts) {
         var self = this,
-            state = {
-                innerX: 0,
-                innerY: 0,
-                mouseDown: false,
-                currentValue: false,
-                init: false
-            },
+            state = {innerX: 0, innerY: 0, mouseDown: false, currentValue: false, init: false},
             container = opts.container,
             handle = container.find('a'),
+            handleWidth = handle.css(['borderBottomWidth', 'borderLeftWidth', 'borderRightWidth', 'borderTopWidth']),
             bounds = {};
         this.setBounds = function () {
             var offset = container.offset();
-            return {
-                bottom: offset.top + container.height() - handle.height(),
-                left: offset.left,
-                right: offset.left + container.width() - handle.width(),
-                top: offset.top
-            };
+            return {bottom: offset.top + container.height() - handle.height(), left: offset.left, right: offset.left + container.width() - handle.width(), top: offset.top};
         };
         this.getPercentageByValue = function (value, range) {
             var percentage = 100 * (value - range.min) / (range.max - range.min);
-            percentage = percentage > 100 ? 100 : percentage;
-            percentage = percentage < 0 ? 0 : percentage;
-            return Math.round(percentage);
+            return Math.round((percentage > 100 ? 100 : percentage) < 0 ? 0 : percentage);
         };
         this.getValueByPercentage = function (percent, range) {
-            return Math.round((((range.max - range.min ) / 100) * percent) + range.min);
+            return Math.round((((range.max - range.min) / 100) * percent) + range.min);
         };
         this.handleMove = function (position) {
-            var m = {},
-                percent = 0,
-                value = 0;
-            if(opts.direction === 'horizontal') {
+            var m = {}, b = {}, p = {min: 0, max: (opts.direction === 'horizontal' ? parseInt(handleWidth.borderBottomWidth, 10) : parseInt(handleWidth.borderRightWidth, 10))}, percent = 0, value = 0;
+            if (opts.direction === 'horizontal') {
                 percent = self.getPercentageByValue(position.x, {min: bounds.left, max: bounds.right});
                 value = self.getValueByPercentage(percent, opts.interval);
-                m.left = position.x < bounds.left ? bounds.left : (position.x > bounds.right ? bounds.right : position.x);
+                b.borderLeftWidth = self.getValueByPercentage(percent, p);
+                b.borderRightWidth = parseInt(handleWidth.borderBottomWidth, 10) - b.borderLeftWidth;
+                m.left = (position.x < bounds.left ? bounds.left : (position.x > bounds.right ? bounds.right : position.x)) - b.borderLeftWidth;
             }
-            if(opts.direction === 'vertical') {
+            if (opts.direction === 'vertical') {
                 percent = self.getPercentageByValue(position.y, {min: bounds.top, max: bounds.bottom});
                 value = self.getValueByPercentage(percent, opts.interval);
-                m.top = position.y < bounds.top ? bounds.top : (position.y > bounds.bottom ? bounds.bottom : position.y);
+                b.borderTopWidth = self.getValueByPercentage(percent, p);
+                b.borderBottomWidth = parseInt(handleWidth.borderRightWidth, 10) - b.borderTopWidth;
+                m.top = (position.y < bounds.top ? bounds.top : (position.y > bounds.bottom ? bounds.bottom : position.y)) - b.borderTopWidth;
             }
             if (value >= opts.limits.min) {
-                handle.offset(m);
+                handle.offset(m).css(b);
                 state.currentValue = value;
-                if (typeof opts.slide === 'function')
-                    opts.slide.apply(this, [container, value, percent, m]);
+                if (typeof opts.slide === 'function') {
+                    opts.slide.apply(this, [container, value, percent, {left: m.left + b.borderLeftWidth, top: m.top + b.borderTopWidth}]);
+                }
             }
         };
         this.bindEvents = function () {
@@ -74,7 +65,7 @@
             });
             $d.on("mousemove", function (e) {
                 if (state.mouseDown) {
-                    self.handleMove({x: e.pageX - state.innerX,y: e.pageY - state.innerY });
+                    self.handleMove({x: e.pageX - state.innerX, y: e.pageY - state.innerY });
                 }
                 if (state.init === false) {
                     state.init = true;
